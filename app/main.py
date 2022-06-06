80d4680c15e843c9ff1f77ceff69fcd3615c674d
xfrogz <renattoyague@gmail.com>
2022-06-06 17:07:34 -0500

    Initialize project FastAPI

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def home_view():
+    return {"hello:" "world"}
+
+
+@app.post("/")
+def home_detail_view():
+    return {"hello:" "world"}
